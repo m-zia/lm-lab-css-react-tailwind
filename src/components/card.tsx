a01df41f77aefd1d026749d6b68675048857eab3
M Zia <murtaza_zia@hotmail.co.uk>
2023-09-06 11:20:23 +0100

    created card component for mapping data onto

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -15,16 +15,20 @@ export interface IProperty {
 export const Card: React.FC<IProperty> = ({ property: { title, location, country, property_type, image_urls, description, bedroom_count, bathroom_count, price } }) => {
 
     return (
-        <>
-            <h4>{title}</h4>
-            <p>{location}</p>
-            <p>{country}</p>
-            <p>{property_type}</p>
-            <p>{description}</p>
-            <p>{bedroom_count}</p>
-            <p>{bathroom_count}</p>
-            <p>{price}</p>
-            <img src={image_urls[0]} />
-        </>
+        <div className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl m-3">
+            <div className="md:flex">
+                <div className="md:flex-shrink-0">
+                    <img className="h-48 w-full object-cover md:w-48" src={image_urls[0]} alt={title} />
+                </div>
+                <div className="p-8">
+                    <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">{property_type}</div>
+                    <h4 className="block mt-1 text-lg leading-tight font-medium text-black">{title}</h4>
+                    <p className="mt-2 text-gray-500">{description}</p>
+                    <p className="mt-2 text-gray-500">{location}, {country}</p>
+                    <p className="mt-2 text-gray-500">Bedrooms: {bedroom_count}, Bathrooms: {bathroom_count}</p>
+                    <p className="mt-2 text-gray-500">{price}</p>
+                </div>
+            </div>
+        </div>
     )
 }
